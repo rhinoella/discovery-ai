26abbc7221344672bbd31dfb226802b93204acac
Noella Spitz <noella.spitz@gmail.com>
2024-06-23 12:18:12 +0100

    Add upload files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
-        <header className="flex flex-row">
-            <p>eee</p>
+        <header className="flex flex-row justify-start w-full text-xl font-medium border-b pt-8 px-12 pb-4 text-slate-600">
+            <h1>Discovery AI</h1>
         </header>
     )
 }
